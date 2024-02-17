a64e9e23c4c5940f0af4ee9837418bb5ba3bc913
Nur Alam <hanako.yugiamane101@gmail.com>
2024-02-17 15:19:55 +0600

    Created Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+import * as DaisyUI from 'daisyui'
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [DaisyUI],
+  // daisyui: {
+  //   themes: ['light']
+  // }
+}
+
